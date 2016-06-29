01b6ef43f7ba47ef8f55ae8425bd20b91fc124ea
Christopher Sfalanga <kitoper.kurisu@gmail.com>
2016-06-29 16:27:41 -0400

    added rating function to ai.cpp

--- ai.cpp
+++ ai.cpp
@@ -1,3 +1,8 @@
+// + ratings are good for player and - ratings are good for opponent
+int rateBoard(char player) {
+    int score = 0;
+}
+
 //drops into leftmost available column
 int makeBasicMoveAI(char player) {
     for(int i = 0; i < 7; ++i) {
