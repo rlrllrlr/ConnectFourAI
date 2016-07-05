9b2a74497a1b306c3679d191880290ce82333111
Christopher Sfalanga <kitoper.kurisu@gmail.com>
2016-07-05 09:31:58 -0400

    started row check in rateboard(ai.cpp)

--- ai.cpp
+++ ai.cpp
@@ -1,6 +1,12 @@
 // + ratings are good for player and - ratings are good for opponent
 int rateBoard(char player) {
     int score = 0;
+
+    //check rows
+    for(int col = 0; col < 6; ++col) {
+        for(int row = 0; row < 7; ++row) {
+        }
+    }
 }
 
 //drops into leftmost available column
