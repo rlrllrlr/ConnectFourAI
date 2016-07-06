e362c3222b276ce5382523933ef7fc3c8323b510
kitoper <kitoper.kurisu@gmail.com>
2016-07-06 10:57:39 -0400

    finished (for now) rateBoard(ai.cpp)

--- ai.cpp
+++ ai.cpp
@@ -40,6 +40,105 @@ int rateBoard(char player) {
     }
 
     //check columns
+    for(int col = 0; col < 7; ++col) {
+        for(int row = 0; row < 6-3; ++row) {
+            int player_counter = 0, opp_counter = 0;
+            for(int a = 0; a < 4; ++a) {
+                if(board[col][row+a] == player) {
+                    player_counter += 1;
+                }
+                if(board[col][row+a] == opp) {
+                    opp_counter += 1;
+                }
+            }
+
+            if(player_counter > 0 && opp_counter > 0) {
+                continue;
+            }
+            else if(player_counter > 0) {
+                if(player_counter >= 4) {
+                    return 8000;
+                }
+
+                score += player_counter*10;
+            }
+            else if(opp_counter > 0) {
+                if(opp_counter >= 4) {
+                    return -8000;
+                }
+
+                score -= opp_counter*10;
+            }
+        }
+    }
+
+    //check down right diagonal
+    for(int col = 0; col < 7-3; ++col) {
+        for(int row = 0; row < 6-3; ++row) {
+            int player_counter = 0, opp_counter = 0;
+            for(int a = 0; a < 4; ++a) {
+                if(board[col+a][row+a] == player) {
+                    player_counter += 1;
+                }
+                if(board[col+a][row+a] == opp) {
+                    opp_counter += 1;
+                }
+            }
+
+            if(player_counter > 0 && opp_counter > 0) {
+                continue;
+            }
+            else if(player_counter > 0) {
+                if(player_counter >= 4) {
+                    return 8000;
+                }
+
+                score += player_counter*10;
+            }
+            else if(opp_counter > 0) {
+                if(opp_counter >= 4) {
+                    return -8000;
+                }
+
+                score -= opp_counter*10;
+            }
+        }
+    }
+
+    //check down left diagonal
+    for(int col = 3; col < 7; ++col) {
+        for(int row = 0; row < 6-3; ++row) {
+            int player_counter = 0, opp_counter = 0;
+            for(int a = 0; a < 4; ++a) {
+                if(board[col-a][row+a] == player) {
+                    player_counter += 1;
+                }
+                if(board[col+a][row+a] == opp) {
+                    opp_counter += 1;
+                }
+            }
+
+            if(player_counter > 0 && opp_counter > 0) {
+                continue;
+            }
+            else if(player_counter > 0) {
+                if(player_counter >= 4) {
+                    return 8000;
+                }
+
+                score += player_counter*10;
+            }
+            else if(opp_counter > 0) {
+                if(opp_counter >= 4) {
+                    return -8000;
+                }
+                
+                score -= opp_counter*10;
+            }
+        }
+    }
+
+    return score;
 }
 
 //drops into leftmost available column
